104b9f6be1002d71f7dda9222b63869d8ffb9d2a
chandan7483 <cschandan187@gmail.com>
2023-09-26 19:43:59 +0530

    hello all

---
+++ fullstack/frontend/angular/angular-demo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutpageComponent } from './pages/aboutpage/aboutpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    AboutpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
